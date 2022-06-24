5f199918d4ae91ccba36708bf0ab6f2d8e34fe31
Martin <luohuafengyue@163.com>
2022-06-25 02:39:13 +0800

    我的藏品VM

--- my/src/main/java/com/sbnh/my/viewmodel/MyCollectionViewModel.kt
+++ my/src/main/java/com/sbnh/my/viewmodel/MyCollectionViewModel.kt
@@ -1,6 +1,13 @@
 package com.sbnh.my.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.sbnh.comm.base.viewmodel.BaseViewModel
+import com.sbnh.comm.entity.base.BasePagerEntity
+import com.sbnh.comm.entity.my.MyCollectionEntity
+import com.sbnh.comm.entity.request.RequestPagerListEntity
+import com.sbnh.my.MyService
+import kotlinx.coroutines.launch
 
 /**
  * 作者: 胡庆岭
@@ -8,5 +15,20 @@ import com.sbnh.comm.base.viewmodel.BaseViewModel
  * 更新时间: 2022/6/14 16:58
  * 描述:我的藏品ViewModel
  */
-class MyCollectionViewModel :BaseViewModel(){
+class MyCollectionViewModel : BaseViewModel() {
+
+    val mCollectionLiveData = MutableLiveData<BasePagerEntity<List<MyCollectionEntity>>>()
+
+    fun loadCollectionList(requestPagerListEntity: RequestPagerListEntity) {
+        viewModelScope.launch {
+            try {
+                val result = mRetrofitManger.create(MyService::class.java)
+                    .loadMyCollectionList(requestPagerListEntity)
+                disposeRetrofit(mCollectionLiveData, result)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
